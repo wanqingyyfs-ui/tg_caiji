e50e4e95ef39caf18a331c5443ec82806cebeb6c
万能青年 <wanqingyyfs@gmail.com>
2026-06-13 22:12:35 +0700

    Update one-click startup command

--- start_all.bat
+++ start_all.bat
@@ -2,5 +2,5 @@
 chcp 65001 >nul
 cd /d "%~dp0"
 if exist ".venv\Scripts\activate.bat" call ".venv\Scripts\activate.bat"
-python -m collector.run_all --open-browser --backfill-on-start --backfill-limit 500 --enrich-limit 300 --enrich-interval 180
+python -m collector.run_all --open-browser --enrich-limit 300 --enrich-interval 180
 pause
