2054664ebfe9ad075046bd97b20858a7a608baea
万能青年 <wanqingyyfs@gmail.com>
2026-06-13 18:11:25 +0700

    Add Windows one-click startup script

---
+++ start_all.bat
@@ -0,0 +1,6 @@
+@echo off
+chcp 65001 >nul
+cd /d "%~dp0"
+if exist ".venv\Scripts\activate.bat" call ".venv\Scripts\activate.bat"
+python -m collector.run_all --open-browser --backfill-on-start --backfill-limit 500 --enrich-limit 300 --enrich-interval 180
+pause
